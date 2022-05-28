ea5ba83d6f30bf7a9723db57f3bcb54f07b75300
Federico Devigili <federico.devigili@tol.info>
2022-05-28 16:57:35 +0200

    Fix missing header in GCC

--- source/ast/datatype.h
+++ source/ast/datatype.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <map>
-#include <vector>
 #include <functional>
+#include <map>
+#include <string>
+#include <vector>
 
 #include "visitormacros.h"
 #include "expressionvisitor.h"
